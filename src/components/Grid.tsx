59ad2a52925bc75e3191c4313374af01348594a8
Jakub Biesiada <biesiadajakub@icloud.com>
2020-11-17 18:19:37 +0100

    refactor: update props order

--- src/components/Grid.tsx
+++ src/components/Grid.tsx
@@ -7,7 +7,7 @@ import { Col } from './Col';
 interface Props extends HTMLProps<HTMLDivElement> {}
 
 export const Grid = ({ children, className, ...props }: Props) => (
-  <div {...props} className={cn('ms-Grid', className)}>
+  <div className={cn('ms-Grid', className)} {...props}>
     {children}
   </div>
 );
