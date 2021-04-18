36e4d146f13eb730b3f5c22c938c48bae2ea4671
Jakub Biesiada <biesiadajakub@icloud.com>
2021-04-18 16:23:11 +0200

    chore: update

--- src/components/Grid.tsx
+++ src/components/Grid.tsx
@@ -1,7 +1,7 @@
 import React, { HTMLProps } from 'react';
 import clsx from 'clsx';
 
-interface Props extends HTMLProps<HTMLDivElement> {}
+export interface Props extends HTMLProps<HTMLDivElement> {}
 
 export const Grid = ({ children, className, ...props }: Props) => (
   <div className={clsx('ms-Grid', className)} {...props}>
--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,24 @@
-import { Grid } from './components/Grid';
+import { Grid, Props } from './components/Grid';
 import { Row } from './components/Row';
 import { Col } from './components/Col';
 
 export { Grid, Row, Col };
 
-Grid.Row = Row;
-Grid.Col = Col;
+interface X extends Props {}
 
-export default Grid;
+// TODO
+const CompoundGrid = Grid as typeof Grid & {
+  Row: typeof Row;
+  Col: typeof Col;
+};
+
+// {
+//   ({ children, className, ...props }: Props): JSX.Element;
+//   Row: any;
+//   Col: any;
+// };
+
+CompoundGrid.Row = Row;
+CompoundGrid.Col = Col;
+
+export default CompoundGrid;
