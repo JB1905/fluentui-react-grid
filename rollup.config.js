a49469aa26850b2574f62c6d7e976cba7b6bb6f2
Jakub Biesiada <biesiadajakub@icloud.com>
2021-05-30 16:21:50 +0200

    chore: update

--- rollup.config.js
+++ rollup.config.js
@@ -21,7 +21,7 @@ export default {
   ],
   plugins: [resolve(), peerDepsExternal(), typescript(), terser()],
   external: [
-    ...Object.keys(pkg.dependencies),
-    ...Object.keys(pkg.peerDependencies),
+    ...Object.keys(pkg.dependencies || {}),
+    ...Object.keys(pkg.peerDependencies || {}),
   ],
 };
