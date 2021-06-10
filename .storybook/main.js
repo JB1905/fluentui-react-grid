4d89b7e82a4c18767ddd04c5a584d42de9ebc33b
Jakub Biesiada <biesiadajakub@icloud.com>
2021-06-10 23:28:26 +0200

    chore: update

--- .storybook/main.js
+++ .storybook/main.js
@@ -7,18 +7,4 @@ module.exports = {
     '@storybook/addon-controls',
     '@storybook/addon-storysource',
   ],
-  webpackFinal: async (config) => {
-    config.module.rules.push({
-      test: /\.(ts|tsx)$/,
-      use: [
-        {
-          loader: require.resolve('ts-loader'),
-        },
-      ],
-    });
-
-    config.resolve.extensions.push('.ts', '.tsx');
-
-    return config;
-  },
 };
