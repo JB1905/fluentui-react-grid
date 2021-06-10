ba80cd01429875ed3bd8968d54312bcb6e029289
Jakub Biesiada <biesiadajakub@icloud.com>
2021-06-10 22:18:53 +0200

    Revert "chore: update (#41)"
    This reverts commit d80b65a0e98239bcd18ac5344ed4762998c732f0.

--- stories/index.stories.tsx
+++ stories/index.stories.tsx
@@ -8,7 +8,7 @@ import { dirs } from '../src/constants/dirs';
 
 import type { Dir } from '../src/types';
 
-import { grid, GridSchema } from './gridSchema';
+import { grid, GridSchema } from '../__mocks__/gridSchema';
 
 interface Props {
   readonly dir: Dir;
